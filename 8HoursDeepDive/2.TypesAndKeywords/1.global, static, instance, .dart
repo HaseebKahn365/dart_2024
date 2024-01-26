7395d4baf9b8d73b93ed149b9f105ed69bb0a758
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-01-26 15:22:09 +0500

    basic terms

---
+++ 8HoursDeepDive/2.TypesAndKeywords/1.global, static, instance, .dart
@@ -0,0 +1,21 @@
+int global = 5; //global
+
+void main(List<String> args) {}
+
+class car {
+  static String type = "car"; //static is common for all;
+  int topspeed = 90; //instance vars
+  late int currentSpeed;
+
+  void reachToSpeed(int s) {
+    if (s > 0 && s < topspeed) {
+      currentSpeed = s;
+    } else {
+      currentSpeed = topspeed;
+    }
+  }
+}
+
+void collide(int cars) {
+  int collided = 3; //local
+}
