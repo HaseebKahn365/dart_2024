c17bec77e97e89d91d1df677944899af9f57eba7
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-02-02 12:39:31 +0500

    generator function

---
+++ 8HoursDeepDive/13.Synchronous Programming/4.iterable.dart
@@ -0,0 +1,34 @@
+void main() {
+  // Iterable is a collection of elements that can be accessed sequentially.
+  // Iterable is an abstract class, so it can't be instantiated directly. and is lazily construted
+  // Iterable is the base class for all the collection classes in dart
+
+  //the iterator is the object that allows us to access the elements of the collection one by one
+  //using the next() method and the current element using the current property. ie. iterator(current, next());
+
+  //an iterator can be used to genenrate a sequence of values, one at a time synchronously.
+
+  var list = genrateAndReturn(); //constructed without being used
+
+  var iterableList = genrateAndReturnIterable(); //not constructed until used
+
+  print("List is $iterableList");
+}
+
+List<int> genrateAndReturn() {
+  final List<int> list = [];
+
+  for (int i = 0; i < 10; i++) {
+    print("adding ${i + 1}");
+    list.add(i + 1);
+  }
+
+  return list;
+}
+
+Iterable<int> genrateAndReturnIterable() sync* {
+  for (int i = 0; i < 10; i++) {
+    print("yielding ${i + 1}");
+    yield i + 1;
+  }
+}
